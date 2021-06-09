3c6bece3c18d12a7338f7053feddeb258f4d1d70
Deepak Singh <deepaksingh@Deepaks-MacBook-Air.local>
2021-06-09 18:59:23 +0530

    use book prperty next to booklist property

--- block-BJaaij/code/script.js
+++ block-BJaaij/code/script.js
@@ -1,10 +1,11 @@
 class BookList{
     constructor(){
         this.books = [];
-        this.currentBookIndex;
+        this.currentBookIndex = 0;
     }
     add(arr){
         this.books = arr;
+        return this.books ;
     }
     getCurrentBook(){
         return this.books[this.currentBookIndex];
@@ -21,12 +22,12 @@ class BookList{
 }
 
 class Book{
-    constructor(title,category,author,finishedDate){
+    constructor(title,category,author){
         this.title = title ;
         this.category = category;
         this.author = author;
         this.isRead = false;
-        this.finishedDate = finishedDate;
+        this.finishedDate = null;
     }
     markBookAsRead(){
         this.isRead = true;
@@ -34,23 +35,23 @@ class Book{
     }
 
 }
-let book1 = new Book("title1", "category1", "author1", "1st june");
-let book2 = new Book("title2", "category2", "author2", "2nd june");
-let book3 = new Book("title3", "category3", "author3", "3rd june");
-let book4 = new Book("title4", "category4", "author4", "4th june");
-let book5 = new Book("title5", "category5", "author5", "5th june");
-
-let bookArray = [book1, book2, book3, book4, book5]
+let book1 = new Book("title1", "category1", "author1");
+let book2 = new Book("title2", "category2", "author2");
+let book3 = new Book("title3", "category3", "author3");
+let book4 = new Book("title4", "category4", "author4");
+let book5 = new Book("title5", "category5", "author5");
 
 let bookList1 = new BookList();
-bookList1.add(bookArray);
+
+bookList1.add([book1, book2, book3, book4, book5]);
 bookList1.changeCurrentBook(2);
 console.log(bookList1.getCurrentBook().title);
 console.log(bookList1.getNextBook().title);
 console.log(bookList1.getPrevBook().title);
 
-console.log(bookList1.getCurrentBook().isRead)
+console.log(bookList1.getCurrentBook().isRead);
 
-book3.markBookAsRead();
+bookList1.getCurrentBook().markBookAsRead();
 
-console.log(bookList1.getCurrentBook().isRead)
+console.log(bookList1.getCurrentBook().isRead);
+console.log(bookList1.getCurrentBook().finishedDate);
